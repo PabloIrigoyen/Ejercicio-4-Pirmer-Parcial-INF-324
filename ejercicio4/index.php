a9c70ee6afce6097c303d5c2c2830eb92e1095dc
PabloIrigoyen <pvillarroeli@fcpn.edu.bo>
2024-05-05 19:43:58 -0400

    Add files via upload

---
+++ ejercicio4/index.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+    <from action="http://localhost:7831/Default.aspx" method="GET">
+        nombre <input type="text" name="nombre"/><br>
+        apellido <input type="text" name="nombre"/><br>
+        <input type="submit" name="Enviar" value="Enviar"/>
+    </from>
+</bodY>
+</html>
